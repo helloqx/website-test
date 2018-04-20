bf31595da18e198cd8b4d98dbe55ac28b464c6aa
QX <teoqixuan@hotmail.com>
2018-04-21 03:07:26 +0800

    Updated index page to song selection page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.forms import SongSelectForm
 from app.models import Song
@@ -7,10 +7,13 @@ from app.models import Song
 @app.route('/index')
 def index():
     form = SongSelectForm()
-    form.song.choices = [(s, s.title) for s in Song.query.order_by('title')]
+    form.song.choices = [(s.id, s.title) for s in Song.query.order_by('title')]
     return render_template('index.html', title='Home', form=form)
 
 @app.route('/display', methods=['GET', 'POST'])
 def display():
-    all_songs = Song.query.all()
+    all_songs = []
+    selected_songs = request.form.getlist('song')
+    for song_id in selected_songs:
+        all_songs.append(Song.query.filter_by(id=song_id).one())
     return render_template('display.html', title='Display', songs=all_songs)
