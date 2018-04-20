ebb72da4db9302828640343f00994a20d97b1ff7
QX <teoqixuan@hotmail.com>
2018-04-21 00:56:57 +0800

    Added display page for displaying chosen songs.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
+from app.models import Song
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/display')
+def display():
+    all_songs = Song.query.all()
+    return render_template('display.html', title='Display', songs=all_songs)
