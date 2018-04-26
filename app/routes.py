8102cb45f2391a2ac5635311ff8c8aa4ce4ef09d
QX <teoqixuan@hotmail.com>
2018-04-26 18:46:49 +0800

    Populate datatable

--- app/routes.py
+++ app/routes.py
@@ -7,9 +7,10 @@ from datetime import datetime
 @app.route('/')
 @app.route('/index')
 def index():
+    all_songs = Song.query.all()
     form = SongSelectForm()
-    form.song.choices = [(s.id, s.title) for s in Song.query.order_by('title')]
-    return render_template('index.html', title='Home', form=form)
+    form.song.choices = [(s.id, s.title) for s in all_songs]
+    return render_template('index.html', title='Home', form=form, songs=all_songs)
 
 @app.route('/display', methods=['GET', 'POST'])
 def display():
@@ -20,4 +21,4 @@ def display():
         for verse in song.lyrics.split("\n\n"):
         	slide = Song(title=song.title, lyrics=verse)
         	all_songs.append(slide)
-    return render_template('display.html', title='Display', songs=all_songs, slide_title='Worship', subtitle=datetime.utcnow())
+    return render_template('display.html', title='Display', slides=all_songs, slide_title='Worship', subtitle=datetime.utcnow())
