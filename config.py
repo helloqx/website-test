6d52c00453faaae1d671831444905dcaa3d0ae2b
QX <teoqixuan@hotmail.com>
2018-05-08 00:34:47 +0800

    Add secret admin password
    for trashy security measure.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
+    SECRET_ADMIN_PASSWORD = os.environ.get('SECRET_ADMIN_PASSWORD') or 'SECRET_ADMIN_PASSWORD'
     IS_HEROKU = os.environ.get('HEROKU') or False
     RESET_DATABASE = os.environ.get('RESET_DATABASE') or False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
