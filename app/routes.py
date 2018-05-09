aa1188c158a72f6bf2c1252f3c99c0b84427b133
QX <teoqixuan@hotmail.com>
2018-05-09 23:11:02 +0800

    Lots of login stuff

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, redirect, flash, url_for
 from app import app, db
-from app.forms import SongSelectForm, SongAddForm, SongEditForm
-from app.models import Song
+from flask_login import current_user, login_user, logout_user, login_required
+from app.forms import SongSelectForm, SongAddForm, SongEditForm, LoginForm, RegistrationForm
+from app.models import Song, User
 from datetime import datetime
-import pytz
+from werkzeug.urls import url_parse
+from pytz import timezone
 
 @app.route('/')
 @app.route('/index')
@@ -31,9 +33,10 @@ def display():
             	slide = Song(title=song.title, lyrics=verse)
             	all_songs.append(slide)
         return render_template('display.html', title='Display', slides=all_songs, 
-                slide_title='Worship', subtitle=datetime.now(pytz.timezone('Asia/Singapore')).date())
+                slide_title='Worship', subtitle=datetime.now(timezone('Asia/Singapore')).date())
 
 @app.route('/add_song', methods=['GET', 'POST'])
+@login_required
 def add_song():
     form = SongAddForm()
     if form.validate_on_submit():
@@ -47,6 +50,7 @@ def add_song():
     return render_template('add_song.html', title='Add a song', form=form)
 
 @app.route('/edit_song', methods=['GET', 'POST'])
+@login_required
 def edit_song():
     all_songs = Song.query.all()
     form = SongEditForm()
@@ -68,3 +72,39 @@ def help():
 @app.route('/contribute')
 def contribute():
     return render_template('contribute.html', title='Contribute')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
